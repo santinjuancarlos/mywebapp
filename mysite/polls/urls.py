a9d9f0fc806deea37b81da58fb6d80577eb898e4
Juan Santin <santinjuancarlos@gmail.com>
2022-10-01 11:31:20 -0500

    Aplicación polls 80%

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     #path('hola2/', views.hola_dos, name='hola2'),
     # ex: /polls/5/
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
